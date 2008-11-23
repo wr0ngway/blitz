7f38253a4b62730a5fd501e14fe2892ce1eda585
Mike Breen <hardbap@gmail.com>
2008-11-23 12:42:10 -0500

    get rid of whitespace in unit test

--- rails_generators/coulda_model/templates/unit_test.rb
+++ rails_generators/coulda_model/templates/unit_test.rb
@@ -5,8 +5,8 @@ class <%= class_name %>Test < ActiveSupport::TestCase
   <% if attribute.reference? -%>
     should_belong_to :<%= attribute.name -%>
     should_have_index :<%= attribute.name -%>_id
-  <% else %>
-    should_have_db_column :<%= attribute.name -%>, :type => "<%= attribute.type -%>"
+  <% else -%>
+    should_have_db_column :<%= attribute.name %>, :type => "<%= attribute.type %>"
   <% end -%>
 <% end -%> 
 end
