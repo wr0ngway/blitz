61ae59a6632f6f519dc3944edf5fe6d1eb8da52f
Dan Croak <dcroak@thoughtbot.com>
2009-03-02 01:27:28 -0500

    updating README with work from today

--- generators/view/view_generator.rb
+++ generators/view/view_generator.rb
@@ -4,7 +4,7 @@ class ViewGenerator < Rails::Generator::NamedBase
       m.directory File.join('app/views', class_path, file_name)
 
       # View template for specified action.
-      if class_path == "new"
+      if actions.include?("new")
         path = File.join('app/views', class_path, file_name, "new.html.erb")
         m.template 'view_new.html.erb', path,
           :assigns => { :action => action, :path => path }
