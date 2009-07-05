5657f3345ca9459e91944100fc567baede307db5
Dan Croak <dcroak@thoughtbot.com>
2009-07-05 18:15:00 -0400

    refactored generated file assertions to be simpler, use new assert_generated_file syntax that takes a file name and expected string contents

--- features/step_definitions/cucumber_steps.rb
+++ features/step_definitions/cucumber_steps.rb
@@ -11,24 +11,20 @@ When /^I generate a "([^\"]*)" feature for "([^\"]*)"$/ do |feature, resource|
 end
 
 Then /^a "posts" feature for the "create" scenario should be generated$/ do
-  assert_generated_file("features/posts.feature") do |body|
-    expected = "  Scenario: Create a new post\n" <<
-               "    Given I am on the new post page\n" <<
-               "    When I create a 'post' named 'A new post'\n" <<
-               "    Then I should see 'A new post'"
-    assert body.include?(expected), 
-      "expected #{expected} but was #{body.inspect}"
+  assert_generated_file("features/posts.feature") do
+    "  Scenario: Create a new post\n"                 <<
+    "    Given I am on the new post page\n"           <<
+    "    When I create a 'post' named 'A new post'\n" <<
+    "    Then I should see 'A new post'"
   end
 end
 
 Then /^a "posts" step definition should be generated$/ do
-  assert_generated_file("features/step_definitions/posts_steps.rb") do |body|
-    expected = "When /^I create a post named \"([^\\\"]*)\"$/ do |name|\n" <<
-               "  fills_in :name, :with => name\n" <<
-               "  click_button 'Create'\n"
-               "end"
-    assert body.include?(expected), 
-      "expected #{expected} but was #{body.inspect}"
+  assert_generated_file("features/step_definitions/posts_steps.rb") do
+    "When /^I create a post named \"([^\\\"]*)\"$/ do |name|\n" <<
+    "  fills_in :name, :with => name\n"                         <<
+    "  click_button 'Create'\n"
+    "end"
   end
 end
 
